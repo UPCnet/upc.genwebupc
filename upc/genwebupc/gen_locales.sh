9dcaced272dbe00aa964693672e1ccbc1442e696
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-06-05 08:10:28 +0000

    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@85 6deaf63b-a510-49cf-aad4-8c651776ecce

---
+++ upc/genwebupc/gen_locales.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PRODUCTNAME="upc.salapremsatheme"
+for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do 
+    if test -d $lang/LC_MESSAGES; then 
+        msgfmt -o $lang/LC_MESSAGES/${PRODUCTNAME}.mo $lang/LC_MESSAGES/${PRODUCTNAME}.po; 
+    fi; 
+done
