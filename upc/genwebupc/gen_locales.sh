81f848f3730dad8ab1ad13dae49ca4af4a18011a
roberto.diaz <roberto.diaz@6deaf63b-a510-49cf-aad4-8c651776ecce>
2008-06-06 08:02:53 +0000

    amb locales
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@88 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupc/gen_locales.sh
+++ upc/genwebupc/gen_locales.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PRODUCTNAME="upc.salapremsatheme"
+PRODUCTNAME="upc.genwebupc"
 for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do 
     if test -d $lang/LC_MESSAGES; then 
         msgfmt -o $lang/LC_MESSAGES/${PRODUCTNAME}.mo $lang/LC_MESSAGES/${PRODUCTNAME}.po; 
