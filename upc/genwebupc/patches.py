2b2ada4a3790e672a8e252eedc2deee97a9c97e5
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-01-16 12:33:37 +0000

    comentari
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@723 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupc/patches.py
+++ upc/genwebupc/patches.py
@@ -2,3 +2,5 @@ from Products.Poi import config
 
 config.ISSUE_MIME_TYPES = ('text/x-web-intelligent', 'text/html')
 config.DEFAULT_ISSUE_MIME_TYPE = 'text/html'
+
+# Parchejat tambe el GenericSetup en en upc.genwebupctheme/upc/genwebupctheme/__init__.py
