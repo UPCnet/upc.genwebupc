5e12c63b64362737474e4fd95d02bc0d5c301168
ramon.navarro-bosch <ramon.navarro-bosch@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-01-29 10:21:11 +0000

    collage extender to copy references
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@777 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupc/content/extender.py
+++ upc/genwebupc/content/extender.py
@@ -3,6 +3,23 @@ from zope.interface import implements
 from Products.Poi.interfaces import IIssue 
 from Products.Archetypes.atapi import StringField
 from archetypes.schemaextender.interfaces import ISchemaModifier
+from Products.Collage.interfaces import ICollageAlias
+
+
+class CollageSchemaModifier(object):
+    """ Modifico el schema del Collage link
+    """
+    implements(ISchemaModifier)
+    adapts(ICollageAlias)
+
+    def __init__(self, context):
+        self.context = context
+   
+    def fiddle(self, schema):
+        #import pdb; pdb.set_trace()
+        schema['target'].keepReferencesOnCopy = True
+
+
 
 class PoiIssueSchemaModifier(object):
     """Modifico el schema del PoiIssue
--- upc/genwebupc/patches.py
+++ upc/genwebupc/patches.py
@@ -3,4 +3,30 @@ from Products.Poi import config
 config.ISSUE_MIME_TYPES = ('text/x-web-intelligent', 'text/html')
 config.DEFAULT_ISSUE_MIME_TYPE = 'text/html'
 
+from Products.Collage.content._alias import CollageAlias
+from Products.ATContentTypes.content.base import ATCTContent
+from Products.Archetypes import atapi
+from Products.ATReferenceBrowserWidget.ATReferenceBrowserWidget import ReferenceBrowserWidget
+from Products.Collage.utilities import CollageMessageFactory as _
+
+CollageAliasSchema = ATCTContent.schema.copy() + atapi.Schema((
+    atapi.ReferenceField(
+        name='target',
+        mutator='set_target',
+        accessor='get_target',
+        relationship='Collage_aliasedItem',
+        multiValued = 0,
+        allowed_types = (),
+        keepReferencesOnCopy = True,
+        widget=ReferenceBrowserWidget(
+            label=_(u'label_alias_target', default="Selected target object"),
+            startup_directory='/',
+        ),
+    ),
+))
+
+CollageAlias.schema = CollageAliasSchema
+
+
+
 # Parchejat tambe el GenericSetup en en upc.genwebupctheme/upc/genwebupctheme/__init__.py
