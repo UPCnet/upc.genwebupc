184429b93a533b7bffa8557bf0084b91e65c5e9b
victor.fernandez <victor.fernandez@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-05-06 16:42:22 +0000

    canvi de versions
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@1054 6deaf63b-a510-49cf-aad4-8c651776ecce

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-version = '3.0.1'
+version = '3.1'
 
 setup(name='upc.genwebupc',
       version=version,
-      description="Paquet de funcionalitats del genwebupc",
+      description="Paquet de funcionalitats de Genweb UPC",
       long_description="""\
 """,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -18,7 +18,7 @@ setup(name='upc.genwebupc',
       keywords='genwebupc genweb upc upcnet',
       author='PloneTeam@UPCnet',
       author_email='plone.team@upcnet.es',
-      url='https://devel.upcnet.es/trac/genwebupc',
+      url='https://genweb.upc.edu/',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['upc'],
