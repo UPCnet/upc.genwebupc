0a135193b3c3874ebd5f818d171466aa1d5704a8
jose.luis.vivanco <jose.luis.vivanco@6deaf63b-a510-49cf-aad4-8c651776ecce>
2009-10-05 14:48:32 +0000

    extender del link
    git-svn-id: https://devel.upcnet.es/svn/genwebupc/trunk/upc.genwebupc@1333 6deaf63b-a510-49cf-aad4-8c651776ecce

--- upc/genwebupc/content/extender.py
+++ upc/genwebupc/content/extender.py
@@ -1,10 +1,17 @@
 from zope.component import adapts
 from zope.interface import implements
 from Products.Poi.interfaces import IIssue 
+
+from Products.Archetypes.atapi import BooleanField
 from Products.Archetypes.atapi import StringField
+from Products.Archetypes.atapi import BooleanWidget
+
 from archetypes.schemaextender.interfaces import ISchemaModifier
 from Products.Collage.interfaces import ICollageAlias
+from Products.ATContentTypes.interface.link import IATLink
 
+from archetypes.schemaextender.field import ExtensionField
+from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 
 class CollageSchemaModifier(object):
     """ Modifico el schema del Collage link
@@ -34,4 +41,39 @@ class PoiIssueSchemaModifier(object):
         schema['details'].widget.rows = 15
         schema['steps'].widget.rows = 6
 
+
+# Any field you tack on must have ExtensionField as its first subclass:
+class _StringExtensionField(ExtensionField, BooleanField):
+    pass
+
+class ATLinkSchemaModifier(object):
+    """Adapter that adds a Ripped Off From Channel field to News Items."""
+    adapts(IATLink)
+    implements(IOrderableSchemaExtender)
+
+    _fields = [
+            _StringExtensionField('Obrirnovafinestra',
+                required=False,
+                searchable=True,
+                widget=BooleanWidget(
+                       label='Open in a new window',
+                       label_msgid='upc.genweb.banners_label_Obrirennovafinestra',
+                       i18n_domain='upc.genweb.banners',
+                )
+            )
+        ]
+
+    def __init__(self, newsItem):
+        pass
+
+    def getFields(self):
+        return self._fields
+
+    def getOrder(self, original):
+        new = original.copy()  # contract requires us to make a new one
+        defaultSchemaFields = new['default']  # fields from the "default" schemata
+        defaultSchemaFields.remove('Obrirnovafinestra')
+        defaultSchemaFields.insert(defaultSchemaFields.index('remoteUrl') + 1,
+                                   'Obrirnovafinestra')  # stick "channel" after "description"
+        return new
 
