54c67975b6d0070707d0324482898c0b10ea0ecf
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2011-11-04 13:59:03 +0100

    update de WS helpers per la migracio

--- upc/genwebupc/browser/helpers.py
+++ upc/genwebupc/browser/helpers.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 from Acquisition import aq_inner
 from App.config import getConfiguration
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from OFS.interfaces import IFolder
+from Products.Five.browser import BrowserView
+from Products.CMFCore.utils import getToolByName
 from five import grok
 from OFS.interfaces import IApplication
 import json
@@ -10,16 +13,14 @@ import json
 DORSALS = {"1": "Víctor Valdés", "2": "Dani Alves", "3": "Piqué", "4": "Cesc", "5": "Puyol", "6": "Xavi", "7": "David Villa", "8": "A. Iniesta", "9": "Alexis", "10": "Messi", "11": "Thiago", "12": "Unknown", "13": "Pinto", "14": "Mascherano", "15": "Keita", "16": "Sergio", "17": "Pedro"}
 
 
-class getZEO(grok.View):
+class getZEO(BrowserView):
     """ Funcio que agafa el numero de zeo al que esta assignat la instancia de genweb.
-        Per això, el buildout s'ha d'afegir una linea a la zope-conf-additional:
+        Per aixo, el buildout s'ha d'afegir una linea a la zope-conf-additional:
         zope-conf-additional =
            <product-config genweb>
                zeo 9
            </product-config>
     """
-    grok.context(IPloneSiteRoot)
-    grok.require('zope2.View')
 
     def dorsal(self):
         config = getConfiguration()
@@ -31,7 +32,17 @@ class getZEO(grok.View):
         return DORSALS[self.dorsal()]
 
 
-class listPloneSites(grok.View):
+def listPloneSites(zope):
+    out = []
+    for item in zope.values():
+        if IFolder.providedBy(item):
+            for site in item.values():
+                if IPloneSiteRoot.providedBy(site):
+                    out.append(site)
+    return out
+
+
+class listPloneSitesView(grok.View):
     grok.name('listPloneSites')
     grok.context(IApplication)
     grok.require('zope2.View')
@@ -45,5 +56,31 @@ class listPloneSites(grok.View):
             if IFolder.providedBy(item):
                 for site in item.values():
                     if IPloneSiteRoot.providedBy(site):
-                        out.append(site.id)
+                        out.append(item.id + '/' + site.id)
         return json.dumps(out)
+
+
+class getFlavourSitesView(grok.View):
+    grok.name('getFlavourSites')
+    grok.context(IApplication)
+    grok.require('zope2.View')
+
+    def render(self):
+        context = aq_inner(self.context)
+        plonesites = listPloneSites(context)
+        out = {}
+        for plonesite in plonesites:
+            portal_skins = getToolByName(plonesite, 'portal_skins')
+            out[plonesite.id] = portal_skins.getDefaultSkin()
+        return json.dumps(out)
+
+
+class getFlavourSiteView(grok.View):
+    grok.name('getFlavourSite')
+    grok.context(IPloneSiteRoot)
+    grok.require('zope2.View')
+
+    def render(self):
+        context = aq_inner(self.context)
+        portal_skins = getToolByName(context, 'portal_skins')
+        return portal_skins.getDefaultSkin()
--- upc/genwebupc/massiveMigrator.py
+++ upc/genwebupc/massiveMigrator.py
@@ -7,24 +7,43 @@
 import requests
 import sys
 import json
+import logging
 
 host = sys.argv[1]
 port = sys.argv[2]
 
-#logger = logging.getLogger('Genweb 4: Migrator')
+user = 'admin'
+password = 'admin'
+
+logger = logging.getLogger('Script Migracio GW4')
+
+skinmap = {'Tema genwebUPC Master': 'GenwebUPC_Master',
+           'Tema genwebUPC Neutre2': 'GenwebUPC_Neutre2',
+           'Tema genwebUPC Neutre3': 'GenwebUPC_Neutre3',
+           'Tema genwebUPC Unitat': 'GenwebUPC_Unitat',
+           'GenwebUPC_Master': 'GenwebUPC_Master',
+           'GenwebUPC_Neutre2': 'GenwebUPC_Neutre2',
+           'GenwebUPC_Neutre3': 'GenwebUPC_Neutre3',
+           'GenwebUPC_Unitat': 'GenwebUPC_Unitat'}
 
 getSites = requests.get("http://%s:%s/@@listPloneSites" % (host, port))
+getSkins = requests.get("http://%s:%s/@@getFlavourSites" % (host, port))
 
-import ipdb; ipdb.set_trace( )
 plonesites = json.loads(getSites.content)
+skins = json.loads(getSkins.content)
+
+logger.info(skins)
 
 for plonesite in plonesites:
     # Purgat + preparacio
-    purgat = requests.get("http://%s:%s/%s/%s/purge" % (host, port, plonesite, plonesite), auth=('admin', 'admin'))
+    purgat = requests.get("http://%s:%s/%s/purge" % (host, port, plonesite), auth=(user, password))
 
     # Upgrade a Plone 4
-    upgrade = requests.post("http://%s:%s/%s/%s/@@plone-upgrade?form.submitted=True&submit=Upgrade" % (host, port, plonesite, plonesite), auth=('admin', 'admin'))
+    upgrade = requests.post("http://%s:%s/%s/@@plone-upgrade?form.submitted=True&submit=Upgrade" % (host, port, plonesite), auth=(user, password))
 
     # Reinstall GW
-    uninstall = requests.post("http://%s:%s/%s/%s/portal_quickinstaller?products%%3Alist=upc.genwebupc&uninstallProducts%%3Amethod=Uninstall" % (host, port, plonesite, plonesite), auth=('admin', 'admin'))
-    install = requests.post("http://%s:%s/%s/%s/portal_quickinstaller/installProducts?products%%3Alist=upc.genwebupc&Install" % (host, port, plonesite, plonesite), auth=('admin', 'admin'))
+    uninstall = requests.post("http://%s:%s/%s/portal_quickinstaller?products%%3Alist=upc.genwebupc&uninstallProducts%%3Amethod=Uninstall" % (host, port, plonesite), auth=(user, password))
+    install = requests.post("http://%s:%s/%s/portal_quickinstaller/installProducts?products%%3Alist=upc.genwebupc&Install" % (host, port, plonesite), auth=(user, password))
+
+    # Reaplica el sabor
+    flavour = requests.get("http://%s:%s/%s/portal_skins/manage_properties?default_skin=%s&request_varname=plone_skin&submit=Save" % (host, port, plonesite, skinmap[skins[plonesite.split('/')[1]]]), auth=(user, password))
